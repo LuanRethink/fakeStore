a434710d08faa6e8c514fa5e99d69df6a3b7171c
LuanRethink <124921888+LuanRethink@users.noreply.github.com>
2023-05-12 15:49:02 -0300

    user route created

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
+import { router as populateRoutes } from "./populate";
 import { router as categoriesRoutes } from "./categories";
 import { router as productsRoutes } from "./products";
-import { router as populateRoutes } from "./populateDatabase";
+import { router as loginRoutes } from "./login";
 import { Router } from "express";
 
 const router: Router = Router();
@@ -8,5 +9,6 @@ const router: Router = Router();
 router.use("/categories", categoriesRoutes);
 router.use("/products", productsRoutes);
 router.use("/populate", populateRoutes);
+router.use("/login", loginRoutes);
 
 export { router };
