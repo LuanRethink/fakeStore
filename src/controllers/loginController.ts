8005eaff7be341e1e6abd3fc71695685b41d4160
LuanRethink <124921888+LuanRethink@users.noreply.github.com>
2023-05-12 15:01:20 -0300

    login controller created

---
+++ src/controllers/loginController.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from "express";
+import loginService from "../service/loginService";
+
+export type User = {
+  id?: number;
+  login: string;
+  password: string;
+};
+
+const newUser = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const user: User = req.body;
+    const response = await loginService.newUser(user);
+    res.status(200).json({ response });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const login = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const user: User = req.body;
+    const token = await loginService.loginUser(user);
+    res.status(200).json({ token });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default { newUser, login };
