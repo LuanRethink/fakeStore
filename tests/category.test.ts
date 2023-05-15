d0149245adc2a31085009af6b909aa570f34cf17
LuanRethink <124921888+LuanRethink@users.noreply.github.com>
2023-05-15 11:30:18 -0300

    some category tests created, not tested

---
+++ tests/category.test.ts
@@ -0,0 +1,44 @@
+import { describe, expect, jest } from "@jest/globals";
+import categoryRepository from "../src/repository/categoryRepository";
+import categoryService from "../src/service/categoryService";
+import productRepository from "../src/repository/productRepository";
+
+describe("Teste usando as funções do Service de Categorias", () => {
+  it("Insere uma categoria nova", async () => {
+    jest.spyOn(categoryRepository, "selectByName").mockResolvedValueOnce([]);
+    jest.spyOn(categoryRepository, "insert").mockResolvedValueOnce([1]);
+
+    const newCategory = await categoryService.insertCategory({
+      name: "nameCategoryTest",
+    });
+    expect(newCategory).toMatchObject({ id: 1, name: "nameCategoryTest" });
+  });
+
+  it("Atualiza o nome de uma categoria", async () => {
+    jest
+      .spyOn(categoryRepository, "selectById")
+      .mockResolvedValueOnce([{ id: 1, name: "nameCategoryTest" }]);
+    jest.spyOn(categoryRepository, "update").mockRejectedValueOnce(1);
+
+    const updatedCategory = await categoryService.updateCategory(1, {
+      name: "categoryNameUpdated",
+    });
+    expect(updatedCategory).toMatchObject({
+      id: 1,
+      name: "categoryNameUpdated ",
+    });
+  });
+  it("Deleta uma categoria", async () => {
+    jest.spyOn(categoryRepository, "remove").mockRejectedValueOnce({ id: 1 });
+    const categoryDeleted = await categoryService.deleteCategory(1);
+    expect(categoryDeleted).toMatchObject({ id: 1 });
+  });
+
+  it("Encontra uma categoria pelo id", async () => {
+    jest
+      .spyOn(categoryRepository, "selectById")
+      .mockResolvedValueOnce([{ id: 1, name: "categoryName" }]);
+    const returnedCategory = await categoryService.findCategoryById(1);
+    expect(returnedCategory).toMatch("1");
+  });
+});
