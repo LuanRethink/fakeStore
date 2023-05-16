f487323e2eb85878fbe52e279dd5918d1d15df5d
LuanRethink <124921888+LuanRethink@users.noreply.github.com>
2023-05-16 09:52:57 -0300

    removed necessity of tokenValidator on populate routes

--- src/routes/populate.ts
+++ src/routes/populate.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
 import populate from "../controllers/populateController";
-import { tokenVerifier } from "../middlewares/tokenVerify";
 
 const router: Router = Router();
 
-router.post("/categories", tokenVerifier, populate.insertCategories);
-router.post("/products", tokenVerifier, populate.insertProducts);
+router.post("/categories", populate.insertCategories);
+router.post("/products", populate.insertProducts);
 
 export { router };
